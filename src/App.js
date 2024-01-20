e64c8d14468df0b9340e360c2619416fbde2b2ca
moulisujith45 <suchith.m.gadde45@gmail.com>
2024-01-20 13:23:54 +0530

    loginworking

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App(){
         <Route path="/dashboard" element={<Dasbord/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
+        <Route path='/admin' element={<AdminHome/>}/>
       </Routes>
 
     </div>
