e065b6193cc96c9895fc51cd68621c83dcdd47a2
moulisujith45 <suchith.m.gadde45@gmail.com>
2024-01-24 14:56:59 +0530

    navbar is done

---
+++ src/Components/CustomerProfile.js
@@ -0,0 +1,7 @@
+export default function CustomerProfile(){
+    return(
+        <div>
+            <h1>Profile Component</h1>
+        </div>
+    )
+}
---
+++ src/Components/orders.js
@@ -0,0 +1,7 @@
+export default function Orders(){
+    return(
+        <div>
+            <h1>Orders Component</h1>
+        </div>
+    )
+}
