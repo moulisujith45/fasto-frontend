ad4723e7e58f752134d6d3e0460360e09d972526
rohith <rohithappuh@gmail.com>
2024-01-26 14:25:19 +0530

    edited product

--- src/Components/AdminComponent/Category/CategoryList.js
+++ src/Components/AdminComponent/Category/CategoryList.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useEffect, useState} from "react"
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 // import { useNavigate } from "react-router-dom"
 
