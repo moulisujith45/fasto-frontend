83aa13834748dce5dcca9349e2f4d3db4f9eeb83
rohith <rohithappuh@gmail.com>
2024-01-27 13:29:00 +0530

    added product

--- src/actions/cartAction.js
+++ src/actions/cartAction.js
@@ -2,3 +2,18 @@ export const addToCart = (product) => ({
     type: 'ADD_TO_CART',
     payload: product
 })
+
+export const startAddCart = (data) => {
+    return async(dispatch) => {
+        try{
+            const response = await axios.post('/api/user/cart', data, {
+                headers: {
+                    Authorization : localStorage.getItem('token')
+                }
+            })
+            dispatch(addToCart(response.data))
+        } catch(err){
+            console.log(err)
+        }
+    }
+}
